44a9c15bdf75772129dc00b05c83aeaa5e774ed8
riviutec <75706876+riviutec@users.noreply.github.com>
2025-03-05 22:00:22 -0400

    Buen trabajo con el while!!! Menu de opciones

---
+++ while02.py
@@ -0,0 +1,27 @@
+""" crear una funcion de menu para mostrar opciones"""
+def menu():
+    print("1. Suma \n 2. Resta \n 3. Multiplicacion \n 4. Division \n 0. Salir")
+    opcion = input("Ingrese una opcion: ")
+    return opcion
+
+# llamar a la funcion menu
+
+op = True
+while op:
+    opcion = menu()
+    if opcion == "1":
+        print("Suma")
+    elif opcion == "2":
+        print("Resta")
+    elif opcion == "3":
+        print("Multiplicacion")
+    elif opcion == "4":
+        print("Division")
+    elif opcion == "0":
+        print("Salir")
+        op = False
+    else:
+        print("Opcion invalida")
+        op = False
+print("Fin del programa")
+
